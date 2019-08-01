7202c87868005c0864b77e17e2a1567f1959d5aa
Hasegawa Takuya <detteiu.x14th@gmail.com>
2019-08-02 01:42:10 +0900

    Update lxcdiscover.php

--- lxcdiscover.php
+++ lxcdiscover.php
@@ -17,16 +17,16 @@ echo "\t\"data\":[\n";
 
 if ($proxmox === 1) {
     foreach ($array_ctid as $key => $row) {
-                if ($array_ct_status[$key] != "running") {
-                        continue;
-                }
+        if ($array_ct_status[$key] != "running") {
+            continue;
+        }
 
         $name = getVename($row);
         $status = getStatus($row);
 
-                if (is_null($name) || is_null($status)) {
-                        continue;
-                }
+        if (is_null($name) || is_null($status)) {
+            continue;
+        }
 
         echo "\t{\n";
         echo "\t\t\"{#CTID}\":\"${row}\",\n";
@@ -34,7 +34,7 @@ if ($proxmox === 1) {
         echo "\t\t\"{#CTSTATUS}\":\"${status}\",\n";
         echo "\t\t\"{#VENAME}\":\"${hostname}\"\n";
         echo $row !== end($array_ctid) ? "\t},\n" : "\t}\n";
-        }
+    }
 } else {
     foreach ($array_ctid as $key => $row) {
         $status = getStatus($row);
