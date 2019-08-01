281bc228db6aa7bcc8193a44a4f6ed354ec56189
Hasegawa Takuya <detteiu.x14th@gmail.com>
2019-08-02 01:30:01 +0900

    Improve compatibility

--- lxcdiscover.php
+++ lxcdiscover.php
@@ -4,17 +4,29 @@
 $proxmox = checkProxmox();
 $hostname = exec('hostname');
 
-$ctid = shell_exec('/usr/bin/lxc-ls -1');
+$ctid = shell_exec("pct list | awk '{print $1}' | sed -r 's/(.*)/\\1/g' | sed -e '1d'");
 $array_ctid = explode("\n", $ctid);
 $array_ctid = array_splice($array_ctid, 0, count($array_ctid)-1 );
+
+$ct_status = shell_exec("pct list | awk '{print $2}'| sed -e '1d'");
+$array_ct_status = explode("\n", $ct_status);
+$array_ct_status = array_splice($array_ct_status, 0, count($array_ct_status)-1 );
 
 echo "{\n";
 echo "\t\"data\":[\n";
 
 if ($proxmox === 1) {
     foreach ($array_ctid as $key => $row) {
+                if ($array_ct_status[$key] != "running") {
+                        continue;
+                }
+
         $name = getVename($row);
         $status = getStatus($row);
+
+                if (is_null($name) || is_null($status)) {
+                        continue;
+                }
 
         echo "\t{\n";
         echo "\t\t\"{#CTID}\":\"${row}\",\n";
@@ -22,7 +34,7 @@ if ($proxmox === 1) {
         echo "\t\t\"{#CTSTATUS}\":\"${status}\",\n";
         echo "\t\t\"{#VENAME}\":\"${hostname}\"\n";
         echo $row !== end($array_ctid) ? "\t},\n" : "\t}\n";
-    }
+        }
 } else {
     foreach ($array_ctid as $key => $row) {
         $status = getStatus($row);
@@ -40,18 +52,18 @@ echo "}\n";
 
 function getVename($ctid)
 {
-    $name = shell_exec("/bin/cat /etc/pve/lxc/${ctid}.conf | grep \"hostname\" | awk '{print $2}'");
+    $name = exec("/bin/cat /etc/pve/lxc/${ctid}.conf | grep \"hostname\" | awk '{print $2}'");
     return trim($name);
 }
 
 function getStatus($ctid)
 {
-    $state = shell_exec("/usr/bin/lxc-info -s -n ${ctid} | awk '{print $2}'");
+    $state = exec("/usr/bin/lxc-info -s -n ${ctid} | awk '{print $2}'");
     return trim($state);
 }
 
 function checkProxmox()
 {
-    $uname  = shell_exec('uname -r');
+    $uname  = exec('uname -r');
     return strpos($uname, "pve") !== false ? 1 : 0;
 }
